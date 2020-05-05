aaf3360c1a883cfd5b71c24ae4c127da4e25b771
陳以新 <elsie96124@gmail.com>
2020-05-05 14:09:19 +0800

    0505-3

--- 0504file/0504.py
+++ 0504file/0504.py
@@ -6,10 +6,10 @@ wwnum = 0 #count all character
 for line in f: #line
     lnum = lnum + 1
     for i in line:
-        if i not in ['\n']:
-            wwnum += 1
         if i not in ['，','；','。','\n',' ','！','？']:
             wnum += 1
+        if i not in ['\n',' ']:
+            wwnum += 1
 
 print('\n此檔案共',lnum,'行\n')
 print('不含標點符號共',wnum,'字\n')
