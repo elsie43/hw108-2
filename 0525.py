918fd597bb8cf327e65dff85b37c696e780b802c
陳以新 <elsie96124@gmail.com>
2020-05-25 17:27:37 +0800

    0525-2

--- 0525.py
+++ 0525.py
@@ -13,7 +13,8 @@ def minus():
     x = num1.get()-num2.get()  
     if x < 0:
         y = round(math.fabs(x))
-        messagebox.showinfo("WARNING!", "金額不足")
+        r = "金額不足，還差" + str(y) + "元"
+        messagebox.showinfo("WARNING!", r)
         
     else:
         return result.set(x)
@@ -24,7 +25,7 @@ price = Label(window, text="應付NT$", width=30, bg="lightblue") #應付
 n2 = Entry(window, width=50, textvariable=num2) #輸入應付
 change = Label(window, text="找零NT$",width=30, bg="lightblue") #找零 
 n3 = Entry(window, width=50, textvariable=result) #跳出找零
-btn = Button(window, text="確定", fg="blue", command = minus)
+btn = Button(window, text="計算找零", fg="blue", command = minus)
 
 #排版
 receive.grid(row=0, column=0)
